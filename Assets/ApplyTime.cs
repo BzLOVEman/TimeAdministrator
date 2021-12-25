1b2bddc62ca917f23214b4f76506e7f9933791cc
ソラノアナ Soranoana <Inanis_foramen@yahoo.co.jp>
2021-12-26 00:36:14 +0900

    一旦完成
    地球の座標と標準時の対応がしたい

--- Assets/ApplyTime.cs
+++ Assets/ApplyTime.cs
@@ -1,17 +1,10 @@
-// using System.Collections;
-// using System.Collections.Generic;
 using UnityEngine;
-// using UnityEditor.UI;
 using UnityEngine.UI;
 
 public class ApplyTime : MonoBehaviour {
 
 	public Text textObj;
 	public TimeAdministration ta;
-
-	void Start() {
-
-	}
 
 	private void LateUpdate() {
 		textObj.text = ta.Year + "/" + ta.Month + "/" + ta.Day + "\n"
--- Assets/TimeAdministration.cs
+++ Assets/TimeAdministration.cs
@@ -15,26 +15,15 @@ public class TimeAdministration : MonoBehaviour {
 	[SerializeField, Range(0, 59), HeaderAttribute("秒")]
 	private int second;
 	[SerializeField, Range(0, 1000), HeaderAttribute("ミリ秒")]
-	private int millisecond;
+	private double millisecond;
 	[SerializeField, HeaderAttribute("初期値をリアルタイムにする")]
 	private bool useRealTime;
 	[SerializeField, HeaderAttribute("再生速度。0:静止、1:リアルタイム 2～:x倍速 -:逆再生")]
-	private float simulateSpeed;
-	private int flame = 0;
-	private readonly int fps = 50;
-
-	//デバッグ用
-	// private float real;
-	// private float wasReal;
-	//デバッグ用終了
+	private double simulateSpeed;
 
 	private readonly int[] monthEnd = new int[12] { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
 	void Start() {
-		//デバッグ用
-		// real = 0;
-		// wasReal = 0;
-		//デバッグ用終了
 		if (useRealTime) {
 			DateTime now = DateTime.Now;
 			year = now.Year;
@@ -50,15 +39,83 @@ public class TimeAdministration : MonoBehaviour {
 	}
 
 	void Update() {
-		MilliSecond += (int)( Time.deltaTime * 1000 * simulateSpeed );
+		millisecond += Time.deltaTime * 1000f * simulateSpeed;
 		timeCarryUpDown();
+	}
 
+	//時間の繰り上がり、繰り下がりの計算を実施
+	void timeCarryUpDown() {
+		//以下時間が増えた場合
+		if (simulateSpeed > 0) {
+			for (int i = (int)Math.Floor(millisecond / 1000f); 0 < i; i--) {
+				millisecond -= 1000;
+				second++;
+			}
+
+			for (int i = second / 60; 0 < i; i--) {
+				second -= 60;
+				minute++;
+			}
+
+			for (int i = minute / 60; 0 < i; i--) {
+				minute -= 60;
+				hour++;
+			}
+
+			for (int i = hour / 24; 0 < i; i--) {
+				hour -= 24;
+				day++;
+			}
+
+			for (int i = ( day - 1 ) / getMonthend(); 0 < i; i--) {
+				day -= getMonthend();
+				month++;
+			}
+
+			for (int i = ( month - 1 ) / 12; 0 < i; i--) {
+				month -= 12;
+				year++;
+			}
+		}
+
+		//以下時間が減った場合
+		if (simulateSpeed < 0) {
+			for (int i = (int)Math.Floor(millisecond / 1000f); i < 0; i++) {
+				millisecond += 1000;
+				second--;
+			}
+
+			for (int i = (int)Math.Floor(second / 60f); i < 0; i++) {
+				second += 60;
+				minute--;
+			}
+
+			for (int i = (int)Math.Floor(minute / 60f); i < 0; i++) {
+				minute += 60;
+				hour--;
+			}
+
+			for (int i = (int)Math.Floor(hour / 24f); i < 0; i++) {
+				hour += 24;
+				day--;
+			}
+
+			for (int i = (int)Math.Floor(( day - 1 ) / (float)getMonthend()); i < 0; i++) {
+				month--;
+				day += getMonthend();
+			}
+
+			for (int i = (int)Math.Floor(( month - 1 ) / 12f); i < 0; i++) {
+				month += 12;
+				year--;
+			}
+		}
 	}
 
 	//月末の日付を返す
 	int getMonthend() {
 		for (int i = 0; i < monthEnd.Length; i++) {
-			if (month == i + 1) {
+			if (month % 12 == ( i + 1 ) % 12) {
 				//閏年対応
 				if (i + 1 == 2 && year % 4 == 0) {
 					return monthEnd[i] + 1;
@@ -69,75 +126,6 @@ public class TimeAdministration : MonoBehaviour {
 		}
 		//エラー
 		return -1;
-	}
-
-	//時間の繰り上がり、繰り下がりの計算を実施
-	void timeCarryUpDown() {
-		//以下時間が増えた場合
-		if (simulateSpeed > 0) {
-			for (int i = millisecond / 1000; i > 0; i--) {
-				millisecond -= 1000;
-				second++;
-			}
-
-			for (int i = second / 60; i > 0; i--) {
-				second -= 60;
-				minute++;
-			}
-
-			for (int i = minute / 60; i > 0; i--) {
-				minute -= 60;
-				hour++;
-			}
-
-			for (int i = hour / 24; i > 0; i--) {
-				hour -= 24;
-				day++;
-			}
-
-			for (int i = ( day - 1 ) / getMonthend(); i > 0; i--) {
-				day -= getMonthend();
-				month++;
-			}
-
-			for (int i = ( month - 1 ) / 12; i > 0; i--) {
-				month -= 12;
-				year++;
-			}
-		}
-
-		//以下時間が減った場合
-		if (simulateSpeed < 0) {
-			for (int i = millisecond / 1000; i < 0; i++) {
-				millisecond += 1000;
-				second--;
-			}
-
-			for (int i = second / 60; i < 0; i++) {
-				second += 60;
-				minute--;
-			}
-
-			for (int i = minute / 60; i < 0; i++) {
-				minute += 60;
-				hour--;
-			}
-
-			for (int i = hour / 24; i < 0; i++) {
-				hour += 24;
-				day--;
-			}
-
-			for (int i = day / getMonthend(); i <= 0; i++) {
-				month--;
-				day += getMonthend();
-			}
-
-			for (int i = month / 12; i <= 0; i++) {
-				month += 12;
-				year--;
-			}
-		}
 	}
 
 	//以下ゲッターとセッター
@@ -173,10 +161,10 @@ public class TimeAdministration : MonoBehaviour {
 
 	public int MilliSecond {
 		set { this.millisecond = value; }
-		get { return this.millisecond; }
+		get { return (int)this.millisecond; }
 	}
 
-	public float SimulateSpeed {
+	public double SimulateSpeed {
 		set { this.simulateSpeed = value; }
 		get { return this.simulateSpeed; }
 	}
